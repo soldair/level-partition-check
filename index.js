466767012c2c3354bf9d27e94b58b7e455d0ab37
Ryan Day <soldair@gmail.com>
2013-06-30 18:33:47 -0700

    first version of partition-validation

--- index.js
+++ index.js
@@ -1,75 +1,23 @@
+var srange = require('string-range');
+var sublevel = require('level-sublevel');
+// expects a sublevel. 
+// implements range validation on puts into this sublevel.
 
 
-
-
-
-
-
-
-
-var binarysearch = require('binarysearch');
-var through = require('pull-through');
-
-// this extends a leveldb
-// if you attempt to write a key out of the partition it errors?
-//
-// require('level-partition')(db,{start:key,end:key})
-// require('level-partition')(db,[{start:key,end:key},...])
-//
 module.exports = function(db,range){
 
-  var put = db.put;
-  db.put = function(key,val,cb){
-    if(!compare(key)){
-      return process.nextTick(function(){
-        cb(err('batch contained out of range key for this partition. '+o.key,'ERANGE'));
-      });
+  db = sublevel(db);
+  
+  db.pre(function(obj){
+    
+    if (obj.type != 'get' && !srange.satisfies(obj.key,range)) {
+      var e = new Error('key out of range for this partition.');
+      e.code = 'ERANGE';
+      e.range = range;
+      throw e;
     }
-    return put.apply(this,arguments);
-  }
-
-  var batch = db.batch;
-  db.batch = function(ops,cb) {
-    for(var i = 0; i < ops.length; ++i) {
-      if(!compare(ops[i].key)){
-        return process.nextTick(function(){
-          cb(err('batch contained out of range key for this partition. '+o.key,'ERANGE'))
-        });
-      }
-    }
-
-    return batch.apply(this,arguments);
-  }
-
-  var writeStream = db.createWriteStream
-
-  db.createWriteStream = function(opts){
-    // if ether start or end are outside of the range this should fail.
-    var s = through(function(data){
-      if (!compare(o.key)) {
-        this.emit('error',err('key out of range for partition. '+o.key,'ERANGE'))
-        return;
-      }
-
-      this.queue(data);
-    })
-    , ws = writeStream.apply(db,arguments)
-
-    s.pipe(ws);
-
-    return s;
-  }
+  });
 
   return db;
-
-  function compare(key){
-    if(key > range.start && key < range.end) return 
-  }
-
-  function err(msg,code){
-    var e = new Error(msg);
-    e.code = code;
-    return e;
-  }
 
 }
--- test/test.js
+++ test/test.js
@@ -1,35 +1,99 @@
 var test = require('tape')
-, levelPart = require('../index.js')
+, partition = require('../index.js')
 , levelup = require('levelup')
-, memdown = require('memdown')
-
-test("put fails out of range",function(){
-
-});
-
-test("put works in range",function(){
-
-});
-
-test("batch fails if any key is out of range",function(){
-
-});
+, MemDown = require('memdown')
+, factory = function (location) { return new MemDown(location) }
 
 
-test("batch works if keys are in range",function(){
+test("put fails out of range",function(t){
+  var db = levelup('/does/not/matter', { db: factory })
+  , part_ac = partition(db,{start:'a',end:'c'})
+ 
+  t.plan(1);
+ 
+  part_ac.put('z',1,function(e){
+    console.log('error from pre',e);
+    t.equals(e.code,'ERANGE','should get erange error if i attepmt to set z in a range of a-c');
+  });
 
 });
 
+test("put works in range",function(t) {
+  var db = levelup('/does/not/matter', { db: factory })
+  , part_ac = partition(db,{start:'a', end:'c'})
+ 
+  t.plan(3);
 
-test("write stream gets error when you write an out of range key",function(){
+  part_ac.put('a', 1, function(e) {
+    t.ok(!e,'should not get error if at the start of the range (inclusive)');
+  });
 
+  part_ac.put('c', 1, function(e) {
+    t.ok(!e,'should not get error if at the end of the range (inclusive)');
+  });
+
+  part_ac.put('b', 1, function(e) {
+    t.ok(!e,'should not get error if in range');
+  });
 });
 
-//TODO
-test("get fails out of range.",function(){
+test("batch fails if any key is out of range",function(t) {
+  var db = levelup('/does/not/matter', { db: factory })
+  , part_ac = partition(db,{start:'a',end:'c'})
 
+  t.plan(1);
+ 
+  part_ac.batch([
+    {type:"del", key:"d"}
+  ], function(e) {
+    t.equals(e.code,'ERANGE','should get erange error if i attepmt to set z in a range of a-c');
+  });
+ 
 });
 
-test("get works with key in range",function(){
+test("batch works if keys are in range",function(t){
+  var db = levelup('/does/not/matter', { db: factory })
+  , part_ac = partition(db,{start:'a',end:'c'})
 
-})
+  t.plan(1);
+ 
+  part_ac.batch([
+    {type:"del",key:"b"}
+  ],function(e){
+    t.ok(!e,'should not get error if in range');
+  });
+  
+});
+
+test("write stream gets error when you write an out of range key",function(t){
+   var db = levelup('/does/not/matter', { db: factory })
+  , part_ac = partition(db,{start:'a',end:'c'})
+
+  t.plan(1);
+
+  var ws = db.createWriteStream();
+  ws.on('error',function(e){
+    t.equals(e.code,'ERANGE','should get erange error if i attepmt to set d in a range of a-c from write stream');
+  });
+  ws.write({key:'a',value:1});
+  ws.write({key:'b',value:1});
+  ws.write({key:'c',value:1});
+  ws.write({key:'d',value:1});
+ 
+});
+
+
+test("get works even outside of range", function(t) {
+   var db = levelup('/does/not/matter', { db: factory })
+  , part_ac = partition(db,{start:'a',end:'c'})
+
+  t.plan(1);
+  var ms = String(Date.now());
+  db.put('a',ms,function(){
+    part_ac.get('a',function(e,v){
+      t.equals(v,ms,'get should have returned set value');
+    });
+  });
+});
+
+
